7ea5e3658c22f5bc9fe076187131761ea60bfe2d
Essa_alalwan <essa.alalwan26@gmail.com>
2026-04-05 00:36:17 +0300

    enable web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import ErrorBoundary from "./components/ErrorBoundary";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => (
   <ErrorBoundary>
@@ -20,6 +21,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics />
       </TooltipProvider>
     </ThemeProvider>
   </ErrorBoundary>
